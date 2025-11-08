2bec71780ee8fc8d0a653f21972bfb893cdbd64e
Clansty <i@gao4.pw>
2025-11-08 14:38:30 +0800

    可能是优化

--- src/components/ScrollText/index.tsx
+++ src/components/ScrollText/index.tsx
@@ -1,19 +1,70 @@
-import { defineComponent, PropType, ref, computed } from 'vue';
+import { defineComponent, ref, onMounted, onBeforeUnmount, watch, nextTick } from 'vue';
 import styles from './styles.module.sass';
 
 export default defineComponent({
   props: {
     title: String
   },
-  setup(props, { emit, slots }) {
+  setup(props, { slots }) {
+    const wrapRef = ref<HTMLDivElement>();
+    const itemRef = ref<HTMLDivElement>();
+    const shouldScroll = ref(false);
+    const wrapWidth = ref(0);
 
-    return () => <div title={props.title}>
-      <div class={styles.wrap}>
-        <div class={styles.item}>
-          {slots.default?.()}
+    let resizeObserver: ResizeObserver | null = null;
+
+    const checkOverflow = () => {
+      if (!wrapRef.value || !itemRef.value) return;
+
+      wrapWidth.value = wrapRef.value.offsetWidth;
+      const itemWidth = itemRef.value.scrollWidth;
+
+      if (itemRef.value.innerText.includes('でらっくす譜面'))
+        console.log(wrapWidth.value, itemWidth);
+
+      shouldScroll.value = itemWidth > wrapWidth.value;
+    };
+
+    onMounted(() => {
+      nextTick(() => {
+        checkOverflow();
+
+        // 监听尺寸变化
+        if (wrapRef.value && typeof ResizeObserver !== 'undefined') {
+          resizeObserver = new ResizeObserver(() => {
+            checkOverflow();
+          });
+          resizeObserver.observe(wrapRef.value);
+        }
+      });
+    });
+
+    onBeforeUnmount(() => {
+      if (resizeObserver) {
+        resizeObserver.disconnect();
+        resizeObserver = null;
+      }
+    });
+
+    // 监听插槽内容变化
+    watch(() => slots.default?.(), () => {
+      nextTick(() => {
+        checkOverflow();
+      });
+    }, { flush: 'post' });
+
+    return () => (
+      <div title={props.title}>
+        <div ref={wrapRef} class={styles.wrap}>
+          <div ref={itemRef} class={[
+            styles.item,
+            shouldScroll.value && styles.scrolling
+          ]} style={{ '--wrap-width': wrapWidth.value + 'px' }}>
+            {slots.default?.()}
+          </div>
         </div>
       </div>
-    </div>;
+    );
   },
 });
 
