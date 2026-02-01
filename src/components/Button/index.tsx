2a2fb50a26dc19da44ba219c79371f4734a5c64e
Clansty <i@gao4.pw>
2026-02-01 23:51:32 +0800

    按钮 onContextmenu

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,12 +5,13 @@ import { defineComponent, PropType } from 'vue';
 export default defineComponent({
   props: {
     onClick: Function as PropType<() => any>,
+    onContextmenu: Function as PropType<() => any>,
     disabled: Boolean,
     ing: Boolean,
   },
   setup(props, { emit, slots }) {
 
-    return () => <button class="relative of-hidden" disabled={props.disabled || props.ing} onClick={props.onClick}>
+    return () => <button class="relative of-hidden" disabled={props.disabled || props.ing} onClick={props.onClick} onContextmenu={props.onContextmenu}>
       <div class="z-2 inline-flex items-center justify-center">{slots.default?.()}</div>
       {props.ing && <LoadingBackground class={['absolute bottom-0 left-0 right-0 top-25% z-1', theme.value.loadingBackground]} />}
     </button>;
