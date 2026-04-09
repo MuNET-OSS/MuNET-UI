ad1b77ddbb1b69f8138f31317d1fb17546c004bc
Clansty <i@gao4.pw>
2026-04-09 12:57:53 +0800

    fix: Radio 组件改为完全受控，防止点击后状态不一致

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -1,22 +1,24 @@
-import { defineComponent, PropType, ref, computed, useId } from 'vue';
-import { useVModel } from '@vueuse/core';
+import { defineComponent, PropType, useId } from 'vue';
 
 export default defineComponent({
   props: {
     onClick: Function as PropType<() => any>,
     value: [String, Number, Boolean, Symbol],
     k: { type: [String, Number, Boolean, Symbol], required: true },
+    disabled: Boolean,
   },
   setup(props, { emit, slots }) {
     const id = useId();
-    const value = useVModel(props, 'value', emit);
 
     return () => <div class="flex gap-2 items-center">
       <input
-        type="radio" id={id} value={props.k} checked={value.value === props.k}
-        onChange={() => {
-          console.log('radio changed', props.k);
-          value.value = props.k;
+        type="radio" id={id}
+        checked={props.value === props.k}
+        disabled={props.disabled}
+        onClick={(e: Event) => {
+          e.preventDefault();
+          if (props.disabled) return;
+          emit('update:value', props.k);
           props.onClick?.();
         }}
       />
