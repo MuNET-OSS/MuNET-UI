d56094351bfdec7785a78c3754c27c769d5ad646
Clansty <i@gao4.pw>
2026-05-17 15:18:34 +0800

    feat: add title prop to Button component and disabled prop to NumberInput component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -11,6 +11,7 @@ export default defineComponent({
     variant: String as PropType<'primary' | 'secondary' | 'ghost'>,
     danger: Boolean,
     size: String as PropType<'small' | 'medium'>,
+    title: String,
   },
   setup(props, { emit, slots }) {
     const getVariantClass = () => {
@@ -43,7 +44,7 @@ export default defineComponent({
       return '';
     };
 
-    return () => <button class={['relative of-hidden', getVariantClass(), getSizeClass()]} disabled={props.disabled || props.ing} onClick={props.onClick} onContextmenu={props.onContextmenu}>
+    return () => <button class={['relative of-hidden', getVariantClass(), getSizeClass()]} disabled={props.disabled || props.ing} onClick={props.onClick} onContextmenu={props.onContextmenu} title={props.title}>
       <div class="z-2 inline-flex items-center justify-center">{slots.default?.()}</div>
       {props.ing && <LoadingBackground class={['absolute bottom-0 left-0 right-0 top-25% z-1', theme.value.loadingBackground]} />}
     </button>;
--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -10,6 +10,7 @@ export default defineComponent({
     step: { type: Number, default: 1 },
     decimal: { type: Number, default: 0 },
     value: { type: Number, default: 0 },
+    disabled: Boolean,
     onChange: Function as PropType<() => void>,
     onFocus: Function as PropType<() => void>,
     innerClass: { type: String, default: 'min-h-12 cst' },
@@ -40,6 +41,6 @@ export default defineComponent({
       refresh();
     }, { immediate: true });
 
-    return () => <TextInput ref={inputRef} v-model:value={wrap.value} type="number" step={props.step} onBlur={commitChange} onEnterPressed={commitChange} onFocus={props.onFocus} innerClass={props.innerClass} />;
+    return () => <TextInput ref={inputRef} v-model:value={wrap.value} type="number" step={props.step} disabled={props.disabled} onBlur={commitChange} onEnterPressed={commitChange} onFocus={props.onFocus} innerClass={props.innerClass} />;
   },
 });
